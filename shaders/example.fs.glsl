71077cc87d8c4e13ce81f20c863a2d446c70bd1b
SashaMKRV <no@no.co>
2020-12-07 21:09:41 -0500

    much with numbers

--- shaders/example.fs.glsl
+++ shaders/example.fs.glsl
@@ -5,11 +5,23 @@ precision mediump float;
   varying vec2 uv;
   void main () {
     float dist = length(gl_FragCoord.xy - mouse);
-    gl_FragColor = vec4(0.98 * texture2D(texture,
-      uv + cos(t) * vec2(0.5 - uv.y, uv.x - 0.5) - sin(2.0 * t) * (uv - 0.5)).rgb, 1) +
+    vec4 temp = vec4(
+        0.90 * texture2D(
+            texture,
+            uv + cos(t) * vec2(0.5 - uv.y, uv.x - 0.5) - sin(2.0 * t) * (uv - 0.5)
+        ).rgb,
+        1
+    ) +
       exp(-0.01 * dist) * vec4(
-        1.0 + cos(2.0 * t),
-        1.0 + cos(2.0 * t + 4.0),
+        1.0 + cos(2.0),
+        1.0 + sin(2.0 * t + 4.0),
         1.0 + cos(2.0 * t + 1.0),
-        0.6);
+        0.6
+    );
+
+    if (gl_FragCoord.x < 400.0) {
+        gl_FragColor = temp * vec4(0.5, 0.5, 0.5, 1);
+    } else {
+        gl_FragColor = temp;
+    }
   }
--- shaders/example.vs.glsl
+++ shaders/example.vs.glsl
@@ -3,5 +3,5 @@ precision mediump float;
   varying vec2 uv;
   void main () {
     uv = position;
-    gl_Position = vec4(2.0 * position - 1.0, 0, 1);
+    gl_Position = vec4(1.6 * position - 0.8, 0, 1.0);
   }
